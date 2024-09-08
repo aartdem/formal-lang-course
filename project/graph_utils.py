1b1fc703901f43048ffc6e2d12a61de75153c1b3
Artem Demchenko <artdem243@gmail.com>
2024-09-08 13:57:17 +0300

    refactor: Reformat file

--- project/graph_utils.py
+++ project/graph_utils.py
@@ -19,10 +19,14 @@ def extract_graph_info(graph_name: str) -> GraphInfo:
     return GraphInfo(
         node_count=graph.number_of_nodes(),
         edge_count=graph.number_of_edges(),
-        edge_labels_list=cfpq_data.get_sorted_labels(graph)
+        edge_labels_list=cfpq_data.get_sorted_labels(graph),
     )
 
 
-def save_two_cycles_graph_to_dot(size1: int, size2: int, label1: str, label2: str, path: str):
-    graph = cfpq_data.labeled_two_cycles_graph(n=size1, m=size2, labels=(label1, label2))
+def save_two_cycles_graph_to_dot(
+    size1: int, size2: int, label1: str, label2: str, path: str
+):
+    graph = cfpq_data.labeled_two_cycles_graph(
+        n=size1, m=size2, labels=(label1, label2)
+    )
     nx.nx_pydot.write_dot(graph, path)
