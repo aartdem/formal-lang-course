0289048105a534530d8cafe7bd1728b3012c2576
Artem Demchenko <artdem243@gmail.com>
2024-09-09 04:24:57 +0300

    test: Add tests for `save_two_cycles_graph_to_dot`

--- tests/test_graph_utils.py
+++ tests/test_graph_utils.py
@@ -1,9 +1,16 @@
+import networkx as nx
 import pytest
 
+from project.graph_utils import (
+    GraphInfo,
+    extract_graph_info,
+    save_two_cycles_graph_to_dot,
+)
 from utils import load_tests_from_json
 
 path_prefix = "tests/test_data/test_graph_utils"
 test_extract_graph_info = "test_extract_graph_info"
+test_save_two_cycles_graph_to_dot = "test_save_two_cycles_graph_to_dot"
 
 
 @pytest.mark.parametrize(
@@ -14,9 +21,8 @@ test_extract_graph_info = "test_extract_graph_info"
     ),
 )
 def test_extract_graph_info(test_data: dict):
-    from project.graph_utils import extract_graph_info, GraphInfo
-
     graph_name = test_data["graph_name"]
+
     if test_data["is_graph_name_supported"]:
         assert True
         expected_info = GraphInfo(
@@ -29,3 +35,36 @@ def test_extract_graph_info(test_data: dict):
     else:
         with pytest.raises(FileNotFoundError):
             extract_graph_info(graph_name)
+
+
+@pytest.mark.parametrize(
+    "test_data",
+    load_tests_from_json(
+        path=f"{path_prefix}/{test_save_two_cycles_graph_to_dot}.json",
+        test_name=test_save_two_cycles_graph_to_dot,
+    ),
+)
+def test_save_two_cycles_graph_to_dot(test_data: dict, tmp_path):
+    size1 = test_data["size1"]
+    size2 = test_data["size2"]
+    label1 = test_data["label1"]
+    label2 = test_data["label2"]
+    path = f"{tmp_path}/{test_data["path"]}"
+
+    if test_data["is_succeed"]:
+        save_two_cycles_graph_to_dot(size1, size2, label1, label2, path)
+        actual_graph = nx.DiGraph(nx.nx_pydot.read_dot(path))
+        expected_graph = nx.DiGraph(
+            list(
+                map(
+                    lambda edge: (edge[0], edge[1], edge[2]),
+                    test_data["edges"],
+                )
+            )
+        )
+
+        assert nx.is_isomorphic(actual_graph, expected_graph, edge_match=dict.__eq__)
+
+    else:
+        with pytest.raises(ValueError):
+            save_two_cycles_graph_to_dot(size1, size2, label1, label2, path)
